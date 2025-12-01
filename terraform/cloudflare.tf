ea627a45868cd832236ccf590e453ae26fe43ebf
Matthew Chenette <mpchenette@github.com>
2025-11-30 20:28:10 -0600

    allowing overwrite instead

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -4,17 +4,19 @@ data "cloudflare_zone" "domain" {
 }
 
 resource "cloudflare_record" "root" {
-  zone_id = data.cloudflare_zone.domain.id
-  name    = "@"
-  content = azurerm_container_app.app.latest_revision_fqdn
-  type    = "CNAME"
-  proxied = true
+  zone_id         = data.cloudflare_zone.domain.id
+  name            = "@"
+  content         = azurerm_container_app.app.latest_revision_fqdn
+  type            = "CNAME"
+  proxied         = true
+  allow_overwrite = true
 }
 
 resource "cloudflare_record" "www" {
-  zone_id = data.cloudflare_zone.domain.id
-  name    = "www"
-  content = var.domain
-  type    = "CNAME"
-  proxied = true
+  zone_id         = data.cloudflare_zone.domain.id
+  name            = "www"
+  content         = var.domain
+  type            = "CNAME"
+  proxied         = true
+  allow_overwrite = true
 }
