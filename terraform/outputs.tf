2b7d875cb8b3ea41a8adfc28eb226baa86136e16
Matthew Chenette <mpchenette@github.com>
2025-11-30 20:24:43 -0600

    changes to add DNS record to terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,10 +14,10 @@ output "resource_group_name" {
   value = azurerm_resource_group.main.name
 }
 
-output "container_registry_name" {
-  value = azurerm_container_registry.acr.name
-}
+# output "container_registry_name" {
+#   value = azurerm_container_registry.acr.name
+# }
 
-output "container_registry_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
+# output "container_registry_login_server" {
+#   value = azurerm_container_registry.acr.login_server
+# }
